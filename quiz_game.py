f90b310a3ffbeebe34be75dd161c898f6f235fdf
mjcarant0 <marjoymcaranto@iskolarngbayan.pup.edu.ph>
2024-10-02 19:26:33 +0800

    Added all quiz questions to the game

---
+++ quiz_game.py
@@ -0,0 +1,7 @@
+#Python Quiz Game
+#list down the questions for the quiz
+questions = ("What programming language is a widely-used, interpreted, object-oriented, and high-level programming language with dynamic semantics, used for general-purpose programming?: ",
+             "Who created python?: ",
+             "What is the date of first released of python?: ",
+             "Where did the name of Python came from?: ", 
+             "The python is maintained by?: ")
