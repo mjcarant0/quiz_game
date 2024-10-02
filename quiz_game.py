d40c60fde45c5603ab3580d370c29fdf05dd6ab8
mjcarant0 <marjoymcaranto@iskolarngbayan.pup.edu.ph>
2024-10-02 20:09:26 +0800

    Added all quiz options to the game

--- quiz_game.py
+++ quiz_game.py
@@ -5,3 +5,9 @@ questions = ("What programming language is a widely-used, interpreted, object-or
              "What is the date of first released of python?: ",
              "Where did the name of Python came from?: ", 
              "The python is maintained by?: ")
+#list down the choices of each questions
+options = (("A. Python", "B. CSS", "C. C++", "D. Java"),
+           ("A. Anders Hejlsberg", "B. Bjarne Stroustrup", "C. Ada Lovelace", "D. Guido van Rossum"),
+           ("A. February 21, 1989", "B. January 21, 1991", "C. February 20, 1991", "D. March 20, 1989"),
+           ("A. Large Snake", "B. Kaa from The Jungle Book", "C. Ekans from Pokémon", "D. Monty Python's Flying Circus"),
+           ("A. Guido van Rossum", "B. Python Software Foundation", "C. Rossum Foundation", "D. Python Programming Association"))
